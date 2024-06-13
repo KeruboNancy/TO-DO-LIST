dc48551a610047a67d02c93317f9ae4ac980263e
KeruboNancy <163673453+KeruboNancy@users.noreply.github.com>
2024-06-13 04:19:31 +0300

    added time function to the js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
         function addTask() {
             
             var taskInput = document.getElementById("task");
+            var taskTime = document.getElementById("task-time")
+
             var taskValue = taskInput.value;
+            var taskTimeValue = taskTime.value;
 
             
             var li = document.createElement("li");
@@ -10,12 +13,15 @@
             checkbox.type = "checkbox";
             li.appendChild(checkbox);
 
-            
-            var textNode = document.createTextNode(taskValue);
-            li.appendChild(textNode);
 
+            var taskTextNode = document.createTextNode(
+              taskValue + " - " + taskTimeValue);
+            li.appendChild(taskTextNode);
+
+            
             var ul = document.getElementById("task-list");
             ul.appendChild(li);
 
             taskInput.value = "";
+            taskTime.value = "";
         }
